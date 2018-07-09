479579f344e33e32ffc86118fa04b4db37d4b716
khaleo hansana <khaleo10@yahoo.com>
2018-07-08 18:47:52 -0600

    this is cool

--- aboutme.js
+++ aboutme.js
@@ -1,3 +1,6 @@
-
+a = "Khaleo Hansana",
+b = 20,
+c = "male",
+bio = "My name is Khaleo Hansana I dabble in the art of code."
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 
